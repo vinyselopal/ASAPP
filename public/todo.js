14a5038538951a9e6dbc160b2a1c46577180863f
viny <viny@debian-BULLSEYE-live-builder-AMD64>
2022-07-14 19:07:16 +0530

    local storage implemented, except checked feature. Checked feature to do from button to checkbox

---
+++ public/todo.js
@@ -0,0 +1,112 @@
+const task = document.querySelector('#task')
+let data = task.value
+const add = document.querySelector('#add')
+
+// on first run, creating list elements stored in local storage
+if (!window.localStorage.getItem('todoItems')) {
+  const todoItemsArr = []
+  const todoItemsArrSerialized = JSON.stringify(todoItemsArr)
+  window.localStorage.setItem('todoItems', todoItemsArrSerialized)
+} else {
+  const getTodoItems = JSON.parse(window.localStorage.getItem('todoItems'))
+  getTodoItems.forEach(a => {
+    const listItem = document.querySelector('ul').lastChild
+    buildElements(a.content, a.id)
+  })
+}
+
+//buildElements called inside buildContainer
+function buildElements (data1, id) {
+  const selectContainer = document.body.querySelector('ul')
+
+  const listItem = document.createElement('li')
+  
+  selectContainer.appendChild(listItem)
+
+  const p = document.createElement('p')
+  const savedTextarea = p.appendChild(document.createElement('textarea'))
+
+  savedTextarea.value = data1
+  savedTextarea.spellcheck = false
+  savedTextarea.className = 'savedTextarea'
+  p.classList.add('savedTask')
+
+  const deleteButton = document.createElement('input')
+
+  const checkbox = document.createElement('input')
+
+  checkbox.type = 'button'
+  checkbox.value = 'strike'
+  checkbox.classList.add('strike')
+
+  deleteButton.type = 'button'
+  deleteButton.value = 'delete'
+  deleteButton.classList.add('delete')
+
+  listItem.appendChild(p)
+  listItem.appendChild(checkbox)
+  listItem.appendChild(deleteButton)
+
+  listItem.dataset.id = id
+
+
+    //checkbox event listener
+    checkbox.addEventListener('click', () => {
+      if (checkbox.value === 'strike') {
+        savedTextarea.classList.add('taskCompletion')
+        checkbox.value = 'unstrike'
+      } else {
+        savedTextarea.classList.remove('taskCompletion')
+        checkbox.value = 'strike'
+      }
+    })
+  
+    //delete-button event listener
+    deleteButton.addEventListener('click', () => {
+      const todoItemsParsed = JSON.parse(window.localStorage.getItem('todoItems'))
+      const filtered = todoItemsParsed.filter(a => a.id !== listItem.dataset.id)
+      window.localStorage.setItem('todoItems', JSON.stringify(filtered))
+      listItem.remove()
+    })
+}
+
+//buildContainer called from buttonClick as callback for event listener on add button
+function buildContainer () {
+  
+  //condition for what to pass as id to build container
+  //and also to increment local storage counter 
+  let uniqueId = Number.parseInt(localStorage.getItem('counter')) 
+  if (uniqueId){
+    buildElements(data, uniqueId)
+    localStorage.setItem('counter', (uniqueId++).toString(10))
+  } else {
+    localStorage.setItem('counter', 1)
+    buildElements(data, 0)
+  }
+  const listItem2 = document.querySelector('ul').lastChild
+
+  //store newly created listItem in local storage
+  const arr = JSON.parse(localStorage.getItem('todoItems'))
+  const obj = {
+    id: listItem2.dataset.id,
+    content: data
+  }
+  console.log(obj)
+  arr.push(obj)
+  window.localStorage.setItem('todoItems', JSON.stringify(arr))
+
+}
+
+// callback function for add, calls buildContainer 
+function buttonClick () {
+  if (task.value) {
+    buildContainer()
+    task.value = ''
+  }
+}
+
+//add event listeners to DOM elements, add and task, on initiation
+task.addEventListener('input', () => {
+  data = task.value
+})
+add.addEventListener('click', buttonClick)
