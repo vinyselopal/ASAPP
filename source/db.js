24220221a82249f1da37f959c47dd7e366b3108e
Viny Selopal <viny0698@gmail.com>
2022-08-01 17:17:30 +0530

    abstracted the database queries

--- source/db.js
+++ source/db.js
@@ -2,10 +2,34 @@ const { Pool } = require('pg')
 const pool = new Pool({
   host: 'localhost',
   port: 5432,
-  // hgjgjhjhkjhjeh
   user: 'postgres',
   database: 'todoapp',
   password: 'abcd'
 })
-
-module.exports = pool
+const findTodoItem = (id) => {
+  return pool.query('SELECT * from todoitems WHERE id = $1', [id])
+}
+const deleteTodoItem = (id) => {
+  return pool.query('DELETE FROM todoitems WHERE id = $1;', [id])
+}
+const deleteAllTodoItems = (id) => {
+  return pool.query('DELETE FROM todoitems;')
+}
+const deleteDoneTodoItems = (id) => {
+  return pool.query('DELETE FROM todoitems WHERE donestatus = true;')
+}
+const updateTodoItem = (parameter, parameterValue, id) => {
+  return pool.query(`UPDATE todoitems SET ${parameter} = $1 WHERE id = $2;`, [parameterValue, id])
+}
+const countDoneTodoItems = (id) => {
+  return pool.query('SELECT COUNT (*)  FROM todoitems WHERE donestatus = true;')
+}
+const getAllTodoItems = (id) => {
+  return pool.query('SELECT * FROM todoitems ORDER BY id ASC;')
+}
+const insertTodoItem = (todoContent, doneStatus, selectedPriority, notes, done) => {
+  return pool.query(
+    'INSERT INTO todoitems (todoContent, doneStatus, selectedPriority, notes, date) VALUES ($1, $2, $3, $4,$5) RETURNING *', [todoContent, doneStatus, selectedPriority, notes, done]
+  )
+}
+module.exports = { findTodoItem, deleteTodoItem, deleteAllTodoItems, deleteDoneTodoItems, updateTodoItem, countDoneTodoItems, getAllTodoItems, insertTodoItem }
--- source/server.js
+++ source/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const database = require('./db.js')
+const { findTodoItem, deleteTodoItem, deleteAllTodoItems, deleteDoneTodoItems, updateTodoItem, countDoneTodoItems, getAllTodoItems, insertTodoItem } = require('./db.js')
 
 // mounting json() middleware converting request to json
 app.use(express.json())
@@ -10,7 +10,7 @@ app.use(express.json())
 // get all todos
 app.get('/todos', async (req, res) => {
   try {
-    const allTodos = await database.query('SELECT * FROM todoitems ORDER BY id ASC;')
+    const allTodos = await getAllTodoItems()
     res.json(allTodos.rows)
   } catch (err) {
     console.error(err)
@@ -21,7 +21,7 @@ app.get('/todos', async (req, res) => {
 // count number of done todos
 app.get('/todos/countDone', async (req, res) => {
   try {
-    const count = await database.query('SELECT COUNT (*)  FROM todoitems WHERE doneStatus = true;')
+    const count = await countDoneTodoItems()
     res.json({ numberOfDoneTasks: parseInt(count.rows[0].count, 10) })
   } catch (err) {
     console.error(err)
@@ -33,7 +33,7 @@ app.get('/todos/countDone', async (req, res) => {
 app.get('/todos/:id', async (req, res) => {
   const { id } = req.params
   try {
-    const todo = await database.query('SELECT * from todoitems WHERE id = $1', [id])
+    const todo = await findTodoItem(id)
     res.json(todo)
   } catch (err) {
     console.error(err)
@@ -45,9 +45,7 @@ app.get('/todos/:id', async (req, res) => {
 app.post('/todos', async (req, res) => {
   try {
     const { todoContent, doneStatus, selectedPriority, notes, done } = req.body
-    const result = await database.query(
-      'INSERT INTO todoitems (todoContent, doneStatus, selectedPriority, notes, date) VALUES ($1, $2, $3, $4,$5) RETURNING *', [todoContent, doneStatus, selectedPriority, notes, done]
-    )
+    const result = await insertTodoItem(todoContent, doneStatus, selectedPriority, notes, done)
     res.json(result.rows[0].id)
   } catch (err) {
     console.error(err)
@@ -60,7 +58,7 @@ app.put('/todos/:id', async (req, res) => {
   try {
     const { id } = req.params
     const parameter = Object.keys(req.body)[0]
-    await database.query(`UPDATE todoitems SET ${parameter} = $1 WHERE id = $2;`, [req.body[parameter], id])
+    await updateTodoItem(parameter, req.body[parameter], id)
     res.json('TODO was updated')
   } catch (err) {
     console.error(err)
@@ -71,7 +69,7 @@ app.put('/todos/:id', async (req, res) => {
 // delete a todo
 app.delete('/todos/clearDone', async (req, res) => {
   try {
-    await database.query('DELETE FROM todoitems WHERE donestatus = true;')
+    await deleteDoneTodoItems()
     res.json('done todos deleted')
   } catch (err) {
     console.error(err)
@@ -83,7 +81,7 @@ app.delete('/todos/clearDone', async (req, res) => {
 app.delete('/todos/:id', async (req, res) => {
   try {
     const { id } = req.params
-    await database.query('DELETE FROM todoitems WHERE id = $1;', [id])
+    await deleteTodoItem(id)
     res.json('TODO was deleted')
   } catch (err) {
     console.error(err)
@@ -94,7 +92,7 @@ app.delete('/todos/:id', async (req, res) => {
 // delete all todos
 app.delete('/todos', async (req, res) => {
   try {
-    await database.query('DELETE FROM todoitems;')
+    await deleteAllTodoItems()
     res.json('All todos were deleted')
   } catch (err) {
     console.err(err)
